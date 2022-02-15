3a47314a82cd667ee8f452dc23de1600708c9034
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-02-15 14:30:20 +0300

    Cart Component finished

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="a table full of delicios food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
