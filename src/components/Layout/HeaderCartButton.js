1ed295dea74e1144c58d78d55e0489edf8b58d2b
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-02-15 16:19:55 +0300

    Shopping cart component added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './HeaderCartButton.module.css';
-import CartIcon from '../Card/CartIcon';
+import CartIcon from '../Cart/CartIcon';
 
 const HeaderCartButton = () => {
   return (
