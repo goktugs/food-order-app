b73933281ba7bb0f85d1e3e7d4fa3b0b1dff02bf
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-02-16 11:14:59 +0300

    Cart context added

---
+++ src/store/cart-context.js
@@ -0,0 +1,10 @@
+import React, { createContext } from 'react';
+
+const CartContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: () => {},
+  removeItem: () => {},
+});
+
+export default CartContext;
